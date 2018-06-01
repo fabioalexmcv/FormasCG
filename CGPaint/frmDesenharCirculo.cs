237ef8dce374eb8013e822bc300112e7e716547c
fabioalexmcv <fa.vasconc@gmail.com>
2018-06-01 00:41:39 -0300

    Version_1.1
    Projeto de CG.
    Fábio e Wallace.

---
+++ CGPaint/frmDesenharCirculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CGPaint {
+    public partial class frmDesenharCirculo : Form {
+        public int Raio { get; set; }
+
+        public frmDesenharCirculo() {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e) {
+            Raio = Convert.ToInt32(numRaio.Value);
+        }
+    }
+}
